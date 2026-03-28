2419ca7d9fa4ed48b8d9c1bb1ed964d7b9511b33
Ankit Kumar <ankitkumar@Ankits-MacBook-Pro-8.local>
2026-03-28 21:10:17 +0530

    displayed products from API

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let url="https://dummyjson.com/products";
+let container=document.getElementById("products")
+
 fetch(url)
 .then(data=>data.json())
 .then((obj)=>{
     let products=obj.products
-    products.forEach(function(ans){
-        console.log(ans.title)
-    })
+    products.forEach(function(item){
+      container.innerHTML += `
+            <div>
+                <img src="${item.thumbnail}" width="150">
+                <h3>${item.title}</h3>
+                <p>Price: ₹${item.price}</p>
+            </div>
+        `;
+    });
 })
+
