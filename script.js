2b25bc91fb92a0fab446b18dd50367bcd951949a
Ankit Kumar <ankitkumar@Ankits-MacBook-Pro-8.local>
2026-03-29 06:11:23 +0530

    Milestone 2 completed - API integration, responsive UI, and loading

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 let url="https://dummyjson.com/products";
 let container=document.getElementById("products")
-
+container.innerHTML = "Loading products...";
 fetch(url)
 .then(data=>data.json())
 .then((obj)=>{
     let products=obj.products
+    container.innerHTML = "";
     products.forEach(function(item){
       container.innerHTML += `
-            <div>
-                <img src="${item.thumbnail}" width="150">
+            <div class="card">
+                <img src="${item.thumbnail}">
                 <h3>${item.title}</h3>
                 <p>Price: ₹${item.price}</p>
             </div>
         `;
+        
     });
+    
 })
+
 
